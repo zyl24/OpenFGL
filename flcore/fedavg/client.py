609191418316fb1e2aca1dfef17d03ab205fe057
zyl24 <2416116991@qq.com>
2024-04-17 22:25:33 +0800

    update

--- flcore/fedavg/client.py
+++ flcore/fedavg/client.py
@@ -17,7 +17,7 @@ class FedAvgClient(BaseClient):
     def send_message(self):
         self.message_pool[f"client_{self.client_id}"] = {
                 "num_samples": self.task.num_samples,
-                "weight": list(self.task.model.parameters()) # 需要变为 list 因为迭代器只能遍历一次
+                "weight": list(self.task.model.parameters())
             }
         
     def personalized_evaluate(self):
