529e72af930feddacbc519d710ef29444c6b7a15
zyl24 <2416116991@qq.com>
2024-05-13 00:41:41 +0800

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,7 +11,7 @@ for dataset in "${datasets[@]}"; do
         for num_client in "${num_clients[@]}"; do
             for (( i=1; i<=repeats; i++ )); do
                 echo "Running python main.py with dataset=${dataset}, fl_algorithm=${fl_algorithm}, num_clients=${num_client}, repeat=${i}"
-                python main.py --dataset "${dataset}" --fl_algorithm "${fl_algorithm}" --num_clients "${num_client}" --scenario fedsubgraph --processing raw --num_rounds 100 --num_epochs 3 --simulation_mode fedsubgraph_louvain --task node_cls --lr 1e-2 --model gcn --metrics accuracy > /dev/null > /dev/null 2>&1
+                python main.py --seed 0 --dataset "${dataset}" --fl_algorithm "${fl_algorithm}" --num_clients "${num_client}" --scenario fedsubgraph --processing raw --num_rounds 100 --num_epochs 3 --simulation_mode fedsubgraph_louvain --task node_cls --lr 1e-2 --model gcn --metrics accuracy > /dev/null > /dev/null 2>&1
             done
         done
     done
