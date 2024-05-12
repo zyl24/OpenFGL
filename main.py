da2f64192c39a2fb58742bd074805abb01351f79
zyl24 <2416116991@qq.com>
2024-05-13 00:41:13 +0800

    update

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from config import args
 from flcore.trainer import FGLTrainer
 from utils.basic_utils import seed_everything
 
-seed_everything(args.seed)
+if args.seed != 0:
+    seed_everything(args.seed)
 trainer = FGLTrainer(args)
 trainer.train()
 
--- view_log.py
+++ view_log.py
@@ -1,14 +1,36 @@
+import os
 import pickle
 
 
-path = "/home/ai2/work/dataset/distrib/fedsubgraph_louvain_1_Cora_client_10/debug/fedavg_2024-05-12_23-48-14.pkl"
 
+# 定义搜索的根目录
+path = "/home/ai2/work/dataset/distrib/fedsubgraph_louvain_1_Cora_client_10/debug/"
 
-with open(path, 'rb') as file:
-    log_file = pickle.load(file)
-    
-    
-print(log_file["time"])
-print(log_file["metric"])
+# 要搜索的字符串
+search_str = "fedavg"
 
-    
+# 初始化一个列表来存储找到的文件路径
+found_files = []
+
+# 使用 os.walk() 遍历目录
+for dirpath, dirnames, filenames in os.walk(path):
+    for filename in filenames:
+        # 检查文件名中是否包含指定的字符串
+        if search_str in filename:
+            # 构造完整的文件路径
+            full_path = os.path.join(dirpath, filename)
+            # 将路径添加到列表中
+            found_files.append(full_path)
+
+# 输出找到的所有文件的路径
+for file_name in found_files:
+    print(file_name)
+
+    path_file_name = os.path.join(path, filename)
+    with open(path_file_name, 'rb') as file:
+        log_file = pickle.load(file)
+        
+    print(log_file["time"])
+    print(log_file["metric"][-1])
+
+        
