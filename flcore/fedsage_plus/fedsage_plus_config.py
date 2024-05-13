60f494f5c74bbc7bdc1edfd4086035bf458e1551
zyl24 <2416116991@qq.com>
2024-05-13 16:12:55 +0800

    update

--- flcore/fedsage_plus/fedsage_plus_config.py
+++ flcore/fedsage_plus/fedsage_plus_config.py
@@ -3,7 +3,6 @@ config = {
     "latent_dim": 128,
     "max_pred": 5,
     "gen_rounds":50,
-    "cls_rounds":50,
     "num_missing_trade_off": 1,
     "missing_feat_trade_off": 1,
     "cls_trade_off": 1
