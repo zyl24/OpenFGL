11b968494b6f6d2ef7ff9bbb5e9939b4f03d2d84
zyl24 <2416116991@qq.com>
2024-05-31 11:42:25 +0800

    update

--- main.py
+++
@@ -1,9 +0,0 @@
-from config import args
-from flcore.trainer import FGLTrainer
-from utils.basic_utils import seed_everything
-
-if args.seed != 0:
-    seed_everything(args.seed)
-trainer = FGLTrainer(args)
-trainer.train()
-
