45b2f8d0fa107377e518c7d2b1acafc73a61d6bd
Youth-49 <youth.049@gmail.com>
2024-06-01 12:24:25 -0400

    modify .readthedocs.yaml and conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,12 +16,24 @@ release = 'v1'
 
 
 extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'recommonmark',
     'sphinx_markdown_tables'
 ]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -32,6 +44,9 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
 
 # sphinx-autobuild source build/html
 todo_include_todos = True
