4a5ad461d8b4668200882db704aa53ea77424194
Youth-49 <youth.049@gmail.com>
2024-06-01 23:44:59 -0400

    modify .readthedocs.yaml and conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
-
+sys.path.insert(0, os.path.abspath('../'))
+# print(sys.path)
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
