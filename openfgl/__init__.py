360e897612995ca92608bcbdee3a006e072afe65
Youth-49 <youth.049@gmail.com>
2024-06-02 00:46:53 -0400

    modify .readthedocs.yaml and conf.py

--- openfgl/__init__.py
+++ openfgl/__init__.py
@@ -7,8 +7,8 @@ This module provides functionality for ...
 
 __version__ = "0.1.0"
 
-from . import data
-from . import model
-from . import flcore
-from . import task
-from . import utils
+# from . import data
+# from . import model
+# from . import flcore
+# from . import task
+# from . import utils
