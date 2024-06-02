2c412c84eb3247a96be85024d5941ed25f434046
Youth-49 <youth.049@gmail.com>
2024-06-02 00:40:40 -0400

    modify .readthedocs.yaml and conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
-sys.path.insert(0, os.path.abspath('../'))
-# print(sys.path)
+sys.path.insert(0, os.path.abspath('../../openfgl'))
+print(sys.path)
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
