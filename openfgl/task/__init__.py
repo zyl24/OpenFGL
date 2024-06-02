d37e861db6b9b3c6d07d85a19af7522b47c79d70
Youth-49 <youth.049@gmail.com>
2024-06-01 23:34:10 -0400

    modify .readthedocs.yaml and conf.py

---
+++ openfgl/task/__init__.py
@@ -0,0 +1,5 @@
+from openfgl.task.base import BaseTask
+from openfgl.task.graph_cls import GraphClsTask
+from openfgl.task.link_pred import LinkPredTask
+from openfgl.task.node_cls import NodeClsTask
+from openfgl.task.node_clust import NodeClustTask
